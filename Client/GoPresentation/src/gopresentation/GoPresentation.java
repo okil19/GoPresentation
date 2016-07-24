5807a3efc9f9ceaaf4e7643e0dc26dc4b1de8fb4
okil19 <lucas.okla@gmail.com>
2016-07-24 21:22:39 +0200

    GoPresentation
    Client

--- Client/GoPresentation/src/gopresentation/GoPresentation.java
+++ Client/GoPresentation/src/gopresentation/GoPresentation.java
@@ -1,7 +1,14 @@
 
 package gopresentation;
 
+import java.awt.Container;
+import java.awt.GridLayout;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+import javax.swing.JMenuItem;
 
 /**
  * @author Lucas
@@ -9,9 +16,29 @@ import javax.swing.JFrame;
 public class GoPresentation extends JFrame {
     
     private static GoPresentation instance = null;
+    public JPanel panel;
+    public JLabel label;
+
+    
+    
     private GoPresentation(){
-        setSize(600, 450);
+        setSize(700, 450);
         setTitle("Go Presentation");
+        setLocation(400, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setVisible(true);
+        
+        panel = new JPanel();
+        panel.setLayout(null);
+        
+        label = new JLabel("Select file to upload:");
+        
+        label.setBounds(10, 10, 150, 25);
+        panel.add(label);
+        
+        Container container = getContentPane();
+        container.add(panel);
     }
     
     public static synchronized GoPresentation getInstance(){
@@ -23,9 +50,6 @@ public class GoPresentation extends JFrame {
 
     public static void main(String[] args) {
         GoPresentation mainWindow = GoPresentation.getInstance();
-        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainWindow.setLocation(400, 200);
-        mainWindow.setVisible(true);
        
     }
     
